a21407b420de2071743a56efe945ac99a291f38c
Enomer <aprehatny@yahoo.com>
2018-04-11 04:24:39 -0700

    Got map to properly show location and render map markers | Passed props of state to HOC

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -28,7 +28,7 @@ const MapWithAMakredInfoWindow = compose(
   withProps({
     googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `400px` }} />,
+    containerElement: <div style={{ height: `100vh` }} />,
     mapElement: <div style={{ height: `100%` }} />,
   }),
   withStateHandlers(() => ({
@@ -43,77 +43,114 @@ const MapWithAMakredInfoWindow = compose(
   withGoogleMap
 )(props => {
   return (
-    <main>
-      { props.apiFinished
-      <GoogleMap
-        defaultZoom={16}
-        defaultCenter={{ lat: 34.146299, lng: -118.255005 }}
-        ref={ref => this.googleMap = ref}
-        {...props}
-        >
-          {props.
-            <Marker
-              position={{ lat: 34.145950, lng: -118.255216 }}
-              onClick={props.onToggleOpen}
-              >
+    <GoogleMap
+      defaultZoom={16}
+      defaultCenter={{lat: props.mylat || 34.146299, lng: props.mylng || -118.255005 }}
+      ref={ref => this.googleMap = ref}
+      {...props}
+      >
+        <Marker
+          position={{lat: 34.146291, lng: -118.255001}}
+        />
+        {props.venues ? this.googleMap && this.googleMap.panTo({lat:props.mylat,lng:props.mylng}) : null}
+        { props.venues ?
+          props.venues
+          .map( (v,i) =>
+          <Marker
+            key={i}
+            position={{ lat: v.location.lat, lng: v.location.lng }}
+            onClick={props.onToggleOpen}
+            >
               {props.isOpen &&
                 <InfoWindow onCloseClick={props.onToggleOpen}>
                   <div> gucci </div>
-              </InfoWindow>}
+                </InfoWindow>
+              }
             </Marker>
-          </GoogleMap>
-        </main>
-      )
-    })
-
-
-    class Home extends Component {
-
-      fetchApi = () => {
-
-        if (navigator.geolocation) {
-          try {
-            navigator.geolocation.getCurrentPosition(position => {
-              const {latitude,longitude} = position.coords
-              const myRequest = fourSquareRequest({
-                ll:latitude+','+longitude
-              })
-              fetch(myRequest).then(response => {
-                if(response.ok){
-                  response.json().then(data => {
-                    this.googleMap && this.googleMap.panTo({lat:latitude,lng:longitude})
-                    callback(null,data)
-                    // console.log(data)
-                  })
-                } else {
-                  callback(new Error("Response not OK"))
-                }
-              }).catch(error => {
-                console.log(error)
-                callback(error)
-              })
-            });
-          } catch(error){
-            console.log(error)
-          }
+          )
+          :
+          null
         }
-  }
 
-      componentDidMount() {
-      }
+      </GoogleMap>
+    )
+  })
 
-      render() {
-        return (
+  class Home extends Component {
 
-          <MapWithAMakredInfoWindow
-            googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCbEXexX7QwrK14aGMnirWoG8sdJe2p8Ds"
-            loadingElement={<div style={{ height: `100%` }} />}
-            containerElement={<div style={{ height: `100vh` }} />}
-            mapElement={<div style={{ height: `100%` }} />}
-          />
-        )
-      }
-
+    state = {
+      venues: null,
+      mylat: null,
+      mylng: null
     }
 
-    export default Home
+    fetchApi = () => {
+      if (navigator.geolocation) {
+        try {
+          console.log('navigator detected')
+          navigator.geolocation.getCurrentPosition(position => {
+            const {latitude,longitude} = position.coords
+            this.setState({
+              mylat:  latitude,
+              mylng: longitude
+            })
+            const myRequest = fourSquareRequest({
+              ll:latitude+','+longitude
+            })
+            console.log('bandos')
+            fetch(myRequest).then(response => {
+              if(response.ok){
+                response.json().then(data => {
+                  this.setState({
+                    venues: data.response.venues
+                  })
+                  callback(null,data)
+                })
+              } else {
+                callback(new Error("Response not OK"))
+              }
+            }).catch(error => {
+              console.log(error)
+              callback(error)
+            })
+          }, err => {
+            // window.location.reload()
+            console.warn(`ERROR(${err.code}): ${err.message}`)
+            return this.fetchApi()
+          },
+          {
+            enableHighAccuracy: true,
+            timeout: 10000,
+            maximumAge: 0
+          })
+        } catch(error){
+          console.log(error)
+        }
+      } else {
+        console.log('navigator gielnor')
+      }
+    }
+
+    componentDidMount() {
+      console.log('gucci')
+      this.fetchApi()
+    }
+
+    render() {
+      return (
+
+        <MapWithAMakredInfoWindow
+          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCbEXexX7QwrK14aGMnirWoG8sdJe2p8Ds"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `100vh` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+          venues={this.state.venues}
+          mylat={this.state.mylat}
+          mylng={this.state.mylng}
+        />
+      )
+    }
+
+  }
+
+  export default Home
