70f5fd9e6a2babbde1682c41c429bb6d1150a2dc
Enomer <aprehatny@yahoo.com>
2018-04-18 22:36:49 -0700

    added comments and indentation correction, cleaned up some styles unused

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -16,7 +16,7 @@ const callback = (err, data) => {
   if (err) return
   console.log(`error occured: ${err}`)
 }
-const formatQueryString = objectofStuff =>  Object.keys(objectofStuff).map(key => key + '=' + objectofStuff[key]).join('&')
+const formatQueryString = objectofStuff =>  Object.keys(objectofStuff).map(key => key + '=' + objectofStuff[key]).join('&')     // API Request my auth tokens
 const fourSquareRequest = (type, params) => "https://api.foursquare.com/v2/venues/" + type + "?" + formatQueryString({
   client_id:"AOYEUOFSLDFJI2A0IRVLHJA0SS0TNS3W1P4AO5USMDJ4AVH2",
   client_secret:"WETOABHTGSZHAJJOXQIJPDEQ52ETJLLMJVJOE4JZVHSEWHDZ",
@@ -26,7 +26,7 @@ const fourSquareRequest = (type, params) => "https://api.foursquare.com/v2/venue
 
 
 
-const MapWithAMakredInfoWindow = compose(
+const MapWithAMakredInfoWindow = compose(   // Higher Order Component for my google map
   withScriptjs,
   withGoogleMap
 )(props => {
@@ -34,24 +34,24 @@ const MapWithAMakredInfoWindow = compose(
     <GoogleMap
       defaultZoom={16}
       defaultOptions={{
-      styles: fancyMapStyles,
-      streetViewControl: false,
-      scaleControl: false,
-      mapTypeControl: false,
-      fullscreenControl: false
-    }}
-      defaultCenter={{lat: props.mylat || 34.146299, lng: props.mylng || -118.255005 }}
+        styles: fancyMapStyles,
+        streetViewControl: false,
+        scaleControl: false,
+        mapTypeControl: false,
+        fullscreenControl: false
+      }}
+      defaultCenter={{lat: props.mylat || 34.146299, lng: props.mylng || -118.255005 }}  // defaults to this location while map fetches your current location
       ref={ref => this.googleMap = ref}
       {...props}
       >
         <Marker
           position={{lat: 34.146291, lng: -118.255001}}
         />
-        {props.venues ? this.googleMap && this.googleMap.panTo({
+        {props.venues ? this.googleMap && this.googleMap.panTo({  // Goes to location of your current location once it loads
           lat:props.mylat,
           lng:props.mylng
         }) : null}
-        { props.placesInfo ?
+        { props.placesInfo ?    // Render 8 map markers of places nearby to your current location
           props.placesInfo
           .map( (v,i) =>
           <GoogleMarker
@@ -74,146 +74,142 @@ const MapWithAMakredInfoWindow = compose(
 class Home extends Component {
 
   state = {
-    venues: null,
-    mylat: null,
-    mylng: null,
-    inputChar: '',
-    venueInfo: null,
-    photoInfo: null,
-    checked: false
+    venues: null,   // manages list of the 8 venues nearest to your
+    mylat: null,  // your latitude
+    mylng: null,    // your longitude
+    inputChar: '',   // manages the search box state
+    venueInfo: null,  // manages info of the venues useful info
+    checked: false    // manages whether side pane is open or not
   }
 
 
 
-inputDetect = (query) => {
-        this.setState({ inputChar: query })
-      }
+  inputDetect = (query) => {
+    this.setState({ inputChar: query })
+  }
 
 
-fetchApi = () => {
-  if (navigator.geolocation) {
-    try {
-      console.log('Getting Current Location')
-      navigator.geolocation.getCurrentPosition(position => {
-        const {latitude,longitude} = position.coords
-        this.setState({
-          mylat:  latitude,
-          mylng: longitude
-        })
-        const myRequest = fourSquareRequest("search", {
-          ll:latitude+','+longitude,
-          venuePhotos:1,
-          venuephotos:"1"
-        })
-        fetch(myRequest).then(response => {
-          if(response.ok){
-            response.json().then(data => {
-              this.setState({
-                venues: data.response.venues.splice(0,8),
-                venueInfo: data.response.venues.splice(0,8).map(ven => [ven.name, ven.location.lat, ven.location.lng, ven.id]),
+  fetchApi = () => {
+    if (navigator.geolocation) {    // finds your current location and stores it in state
+      try {     // try catch for errors
+        console.log('Getting Current Location')
+        navigator.geolocation.getCurrentPosition(position => {
+          const {latitude,longitude} = position.coords
+          this.setState({
+            mylat:  latitude,
+            mylng: longitude
+          })
+          const myRequest = fourSquareRequest("search", {   // fetch request to foursquare api with parameters
+            ll:latitude+','+longitude
+          })
+          fetch(myRequest).then(response => {
+            if(response.ok){
+              response.json().then(data => {
+                this.setState({
+                  venues: data.response.venues.splice(0,8),
+                  venueInfo: data.response.venues.splice(0,8).map(ven => [ven.name, ven.location.lat, ven.location.lng, ven.id]),
+                })
+
+                callback(null,data)
               })
-
-              callback(null,data)
-            })
-          } else {
-            callback(new Error("Response not OK"))
-          }
-        }).catch(error => {
-          console.log(error)
-          callback(error)
+            } else {
+              callback(new Error("Response not OK"))
+            }
+          }).catch(error => {
+            console.log(error)
+            callback(error)
+          })
+        }, err => {
+          console.warn(`ERROR(${err.code}): ${err.message} ... reinitiliazing`)
+          return this.fetchApi()
+        },
+        {
+          enableHighAccuracy: true,   // trying to get geolocation to work on every refresh
+          timeout: 10000,
+          maximumAge: 60000
         })
-      }, err => {
-        console.warn(`ERROR(${err.code}): ${err.message} ... reinitiliazing`)
-        return this.fetchApi()
-      },
-      {
-        enableHighAccuracy: true,
-        timeout: 10000,
-        maximumAge: 60000
-      })
-    } catch(error){
-      console.log(error)
+      } catch(error){
+        console.log(error)
+      }
     }
   }
-}
 
 
-componentDidMount() {
-  this.fetchApi()
-}
+  componentDidMount() {
+    this.fetchApi()   // invokes the fetch call on component mount
+  }
 
-render() {
-  const {venues, mylat, mylng, inputChar, venueInfo, checked} = this.state
-  let placesInfo = null;
+  render() {
+    const {venues, mylat, mylng, inputChar, venueInfo, checked} = this.state
+    let placesInfo = null;
     if (venueInfo) {
       placesInfo = venueInfo
     }
 
     if (inputChar)  {
-      const match = new RegExp(escapeRegExp(inputChar), 'i')
+      const match = new RegExp(escapeRegExp(inputChar), 'i')    // reg exp finds pattern to match side pane places with search box
       placesInfo = venueInfo.filter(
         (place) => match.test(place[0])
       )
     } else {
       placesInfo = venueInfo
     }
-  return (
-    <main>
+    return (
+      <main>
+        <ul className="navigation">
+          <section  className="grid-x">
+            <div id="sidePane" className="align-center cell large-3 medium-4 small-4">
+              <h2 style={{textAlign: 'center', color: '#1f8a70', fontWeight: 'bold'}}>Locations Near You</h2>
+              <input
+                type="text"
+                placeholder="Search Place"
+                value={inputChar}
+                onChange={(event) => {
+                  this.inputDetect(event.target.value)}}
+                />
+                <ul>
+                  {venueInfo ?
+                    placesInfo.map( (v,i) => {
+                      return (
+                        <li
+                          className="nav-item"
+                          key={i}
+                          onClick={() =>
+                            this.setState({
+                              checked: !checked
+                            })
+                          }
+                          >
+                            <hr></hr>
+                            <p id="placesInfo">{placesInfo.map(v => v[0])[i]}</p>
+                          </li>
+                        )
+                      }
+                    )
+                    :
+                    null
+                  }
+                </ul>
+              </div>
+            </section>
 
-      <ul className="navigation">
+          </ul>
+          <input onClick={() => this.setState({checked: !checked})} type="checkbox" checked={checked} id="nav-trigger" className="nav-trigger" />
+          <label htmlFor="nav-trigger"></label>
 
-
-      <section  className="grid-x">
-        <div id="sidePane" className="align-center cell large-3 medium-4 small-4">
-          <h2 style={{textAlign: 'center', color: '#1f8a70', fontWeight: 'bold'}}>Locations Near You</h2>
-          <input
-            type="text"
-            placeholder="Search Place"
-            value={inputChar}
-            onChange={(event) => {
-              this.inputDetect(event.target.value)}}
-            />
-
-        <ul>
-          {
-            venueInfo ?
-            placesInfo.map( (v,i) => {
-                return (
-                  <li onClick={() =>
-                    this.setState({
-                      checked: !checked
-                    })
-                  } className="nav-item" key={i} >
-                      <hr></hr>
-                    <p id="placesInfo">{placesInfo.map(v => v[0])[i]}</p>
-                  </li>
-                )
-              }
-            )
-            :
-            null
-          }
-        </ul>
+          <div className="site-wrap">
+            <MapWithAMakredInfoWindow     // these props are passed up to the higher order component at the top
+            googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCbEXexX7QwrK14aGMnirWoG8sdJe2p8Ds"
+            loadingElement={<div style={{ height: `100%` }} />}
+            containerElement={<div style={{ height: `100vh` }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+            venues={venues}
+            mylat={mylat}
+            mylng={mylng}
+            placesInfo = {placesInfo}
+          />
         </div>
-      </section>
-
-      </ul>
-      <input onClick={() => this.setState({checked: !checked})} type="checkbox" checked={checked} id="nav-trigger" className="nav-trigger" />
-      <label htmlFor="nav-trigger"></label>
-
-<div className="site-wrap">
-      <MapWithAMakredInfoWindow
-        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCbEXexX7QwrK14aGMnirWoG8sdJe2p8Ds"
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `100vh` }} />}
-        mapElement={<div style={{ height: `100%` }} />}
-        venues={venues}
-        mylat={mylat}
-        mylng={mylng}
-        placesInfo = {placesInfo}
-      />
-      </div>
-    </main>
+      </main>
     )
   }
 }
--- src/components/home/infowindow.jsx
+++ src/components/home/infowindow.jsx
@@ -14,76 +14,74 @@ export default class GoogleMarker extends Component {
     locationType: null
   }
 
-  onToggleOpen = ()  => {
-if (!this.state.photoInfo) {
- fetch(
-   this.props.fourSquareRequest( `${this.props.v[3]}`  , {
-     limit: 2
-   })
- ).then(response => {
-     response.json().then(data => {
-       this.setState({
-         photoInfo: data.response.venue.photos.groups.length ? data.response.venue.photos.groups[0].items[0] : null,
-         locationType: data.response.venue.categories[0] ? data.response.venue.categories[0].name : null
-       })
-       console.log(data.response.venue.categories[0] ? console.log('yes') : console.log('no'))
-     }
-   )
-})
+  onToggleOpen = ()  => {         // manages whether infowindow is open and makes a fetch call for details of this particular venue and stores info in state
+    if (!this.state.photoInfo) {
+      fetch(
+        this.props.fourSquareRequest( `${this.props.v[3]}`  , {
+          limit: 2
+        })
+      ).then(response => {
+        response.json().then(data => {
+          this.setState({
+            photoInfo: data.response.venue.photos.groups.length ? data.response.venue.photos.groups[0].items[0] : null,
+            locationType: data.response.venue.categories[0] ? data.response.venue.categories[0].name : null
+          })
+        }
+      )
+    })
+  }
+
+  this.setState({
+    isOpen: !this.state.isOpen   // i dont know how i can make this info window of the marker corresponding to the side pane name open when that particular side pane name is clicked
+  })
 }
 
-this.setState({
-  isOpen: !this.state.isOpen
-})
-}
+render() {
+  const { markerLat, markerLng} = this.props
+  const { isOpen, photoInfo, locationType } = this.state
+  return (
 
-  render() {
-    const { markerLat, markerLng} = this.props
-    const { isOpen, photoInfo, locationType } = this.state
-    return (
-
-      <Marker
-        position={{ lat: markerLat, lng: markerLng }}
-        defaultAnimation={google.maps.Animation.DROP}
-        onClick={this.onToggleOpen}
-        icon={{
+    <Marker
+      position={{ lat: markerLat, lng: markerLng }}
+      defaultAnimation={google.maps.Animation.DROP}   // i have no clue how to animate bounce on click with react google maps couldnt find an answer anywhere
+      onClick={this.onToggleOpen}
+      icon={{
         url:"http://www.myiconfinder.com/uploads/iconsets/48-48-7a195b78d9607a48fb234f98634fa5ea-pin.png"
-        }}
-        >
-          {isOpen &&
-            <InfoWindow
-                onCloseClick={() => this.setState({isOpen: !isOpen})}
-                style={{background: 'orange', color: 'orange' }}
-              >
+      }}
+      >
+        {isOpen &&
+          <InfoWindow
+            onCloseClick={() => this.setState({isOpen: !isOpen})}
+            >
               <div
                 className="grid-x align-middle align-center text-center"
                 style={{maxWidth: '250px'}}
                 >
-              <h5 className="cell">
-                {this.props.placeName}
-              </h5>
-                {
-                  locationType ?
-                  <h6 className="cell">{locationType}</h6>
-                  :
-                  null
-                }
-                {
-                  photoInfo ?
-                 <img
-                   className="cell"
-                   alt={photoInfo.source.name}
-                   src={`${photoInfo.prefix}${photoInfo.height}x${photoInfo.width}${photoInfo.suffix}`}
-                   style={{maxHeight:'200px', maxWidth:'200px'}}
-                 />
-                 :
-                 null
-               }
-              </div>
-            </InfoWindow>
-          }
-        </Marker>
+                  <h5 className="cell">
+                    {this.props.placeName}
+                  </h5>
+                  {
+                    locationType ?
+                    <h6 className="cell">{locationType}</h6>
+                    :
+                    null
+                  }
+                  {
+                    photoInfo ?
+                    <img
+                      className="cell"
+                      alt={photoInfo.source.name}    // type of category listed in inventory window
+                      src={`${photoInfo.prefix}${photoInfo.height}x${photoInfo.width}${photoInfo.suffix}`}   // renders photo stored in state when fetch call is made for it
+                      style={{maxHeight:'200px', maxWidth:'200px'}}
+                    />
+                    :
+                    null
+                  }
+                </div>
+              </InfoWindow>
+            }
+          </Marker>
 
-      )
+        )
+      }
     }
-  }
